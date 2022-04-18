3b01a1a9d9d57f8b55ab6036c99689c4d69a75e0
shalomeiravw <94827164+shalomeiravw@users.noreply.github.com>
2022-04-18 17:00:00 +0700

    Add files via upload

---
+++ tictactoe1/week13/week13/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace week13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNama = new System.Windows.Forms.Label();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.textBoxNama = new System.Windows.Forms.TextBox();
+            this.comboBoxNama = new System.Windows.Forms.ComboBox();
+            this.listBoxNama = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.Location = new System.Drawing.Point(192, 94);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(35, 13);
+            this.labelNama.TabIndex = 0;
+            this.labelNama.Text = "label1";
+            this.labelNama.Click += new System.EventHandler(this.labelNama_Click);
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.Location = new System.Drawing.Point(180, 229);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(75, 23);
+            this.buttonUbah.TabIndex = 1;
+            this.buttonUbah.Text = "Ubah !";
+            this.buttonUbah.UseVisualStyleBackColor = true;
+            this.buttonUbah.Click += new System.EventHandler(this.buttonUbah_Click);
+            // 
+            // textBoxNama
+            // 
+            this.textBoxNama.Location = new System.Drawing.Point(180, 203);
+            this.textBoxNama.Name = "textBoxNama";
+            this.textBoxNama.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNama.TabIndex = 2;
+            // 
+            // comboBoxNama
+            // 
+            this.comboBoxNama.FormattingEnabled = true;
+            this.comboBoxNama.Location = new System.Drawing.Point(333, 201);
+            this.comboBoxNama.Name = "comboBoxNama";
+            this.comboBoxNama.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxNama.TabIndex = 3;
+            // 
+            // listBoxNama
+            // 
+            this.listBoxNama.FormattingEnabled = true;
+            this.listBoxNama.Location = new System.Drawing.Point(561, 162);
+            this.listBoxNama.Name = "listBoxNama";
+            this.listBoxNama.Size = new System.Drawing.Size(120, 95);
+            this.listBoxNama.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxNama);
+            this.Controls.Add(this.comboBoxNama);
+            this.Controls.Add(this.textBoxNama);
+            this.Controls.Add(this.buttonUbah);
+            this.Controls.Add(this.labelNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.Button buttonUbah;
+        private System.Windows.Forms.TextBox textBoxNama;
+        private System.Windows.Forms.ComboBox comboBoxNama;
+        private System.Windows.Forms.ListBox listBoxNama;
+    }
+}
+
---
+++ tictactoe1/week13/week13/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUbah_Click(object sender, EventArgs e)
+        {
+            labelNama.Font = new Font("Arial", 10, FontStyle.Italic);
+            labelNama.Text = textBoxNama.Text;
+            comboBoxNama.Items.Add(textBoxNama.Text);
+            listBoxNama.Items.Add(textBoxNama.Text);
+        }
+
+        private void labelNama_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
